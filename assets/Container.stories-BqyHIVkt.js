aa24db1223530e4aee3d9f53531e00f89c4147d0
jaeheesong-ktc <jaeheesong-ktc@users.noreply.github.com>
2024-12-18 02:17:15 +0000

    deploy: 0ae992f766e30995248e9a7a795067342f7f2602

--- assets/Container.stories-BqyHIVkt.js
+++
@@ -1,5 +0,0 @@
-import{C as n}from"./Container-BulatbKx.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const c={title:"Example/Container",component:n,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{size:{control:"text",description:"The width of the container",defaultValue:void 0},backgroundColor:{control:"color",description:"Background color of the container",defaultValue:void 0},children:{control:"text",description:"Content inside the container"}}},e={args:{children:"This is a container with default styles defined in CSS."}};var t,o,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: 'This is a container with default styles defined in CSS.'
-  }
-}`,...(r=(o=e.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,c as default};
